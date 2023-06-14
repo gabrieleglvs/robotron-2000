733229ec8642da0f19ec90ab781fc5e063ce36e7
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-06-14 15:42:43 -0300

    trabalhando com eventos e funções

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert('oi');
+const robotron = document.querySelector("#robotron")
+
+/*robotron.addEventListener("click", dizOi);
+
+function dizOi() {
+    console.log('oi')
+}*/
+
+robotron.addEventListener("click", () => {
+    console.log('oi');
+})

2885bafaa46469341decad3438447dd1130656fb
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-06-14 17:04:02 -0300

    trabalhando com forEach

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
-somar.addEventListener("click", (e) => {manipulaDados("somar")})
-subtrair.addEventListener("click", (e) => {manipulaDados("subtrair")})
+const controle = document.querySelectorAll(".controle-ajuste")
+console.log(controle)
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (e) => {
+        manipulaDados(e.target.textContent);
+    })
+})
 
 function manipulaDados(operacao) {
-    if(operacao === 'subtrair') {
+    if(operacao === '-') {
         braco.value = parseInt(braco.value) - 1;
     } else {
         braco.value = parseInt(braco.value) + 1;

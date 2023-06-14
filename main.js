c1c358562db01f6b61d31e95902535510741148e
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-06-14 15:53:52 -0300

    trabalhando funcoes e paramentros

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const robotron = document.querySelector("#robotron")
 
-/*robotron.addEventListener("click", dizOi);
-
-function dizOi() {
-    console.log('oi')
-}*/
-
-robotron.addEventListener("click", () => {
-    console.log('oi');
+robotron.addEventListener("click", (e) => {
+    console.log(e);
 })
+
+function dizOi(nome) {
+    console.log("Oi " + nome)
+    console.log('Bem vindo ao Robotron 2000')
+}
+
+dizOi("Pedro")

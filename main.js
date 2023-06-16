bd7e7a8059a4bfaa2fec74507ba0baafccab1556
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-06-16 12:32:11 -0300

    trabalhando com data-attributes

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-const peca = document.querySelector("#braco")
-
-const controle = document.querySelectorAll(".controle-ajuste")
+const controle = document.querySelectorAll("[data-controle]")
 console.log(controle)
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (e) => {
-        manipulaDados(e.target.textContent, e.target.parentNode); //pego o texto e o elemento (controle) de onde veio aquele click (ex: braços, blindagem, núcleos...)
+        manipulaDados(e.target.dataset.controle, e.target.parentNode); //pego o texto e o elemento (controle) de onde veio aquele click (ex: braços, blindagem, núcleos...)
     })
 })
 
 function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector(".controle-contador")
+    const peca = controle.querySelector("[data-contador]")
 
     if(operacao === '-') {
         peca.value = parseInt(peca.value) - 1;
